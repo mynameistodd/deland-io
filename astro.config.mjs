d2bd3f2b0deaa1039a1c4964f5a77c009660a95e
Todd DeLand <todd.deland@gmail.com>
2025-12-01 08:06:28 -0500

    feat: set Astro base path to `/deland-io`

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://deland.io',
+  base: '/deland-io',
   vite: {
     plugins: [tailwindcss()]
   },
